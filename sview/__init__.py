ff362fc44450f01135065efad5d84cff0815bcba
dusktreader <tucker.beck@gmail.com>
2016-12-11 10:02:05 -0800

    Issue #1: Added the ability to copy included files as well

--- sview/__init__.py
+++ sview/__init__.py
@@ -1,5 +1,6 @@
 import flask
 import os
+import re
 import shutil
 
 from livereload import Server
@@ -7,25 +8,65 @@ from sphinx.application import Sphinx
 from textwrap import dedent
 
 
-def build(target, working_dir):
+def find_includes(target_path):
+    includes = []
+    print("reading lines from {}".format(target_path))
+    with open(target_path, 'r') as target_file:
+        for line in target_file.readlines():
+            match = re.search(r'\.\. (?:literal)?include::\s*([^\s]+)', line)
+            if match is not None:
+                print("found a match in in {}".format(line))
+                include_path = match.group(1)
+                print("match was {}".format(include_path))
+                includes.append(include_path)
+    print("includes found: {}".format(includes))
+    return includes
+
+
+def build(target_path, working_dir):
     """
     Rebuilds a target in a specified working directory
+
+    .. todo::
+        - replace .. in paths with 'up' (also in source files)
+        - recursive includes
+        - link copy as well
+        - replace prints with logging
     """
     build_dir = os.path.join(working_dir, 'build')
     source_dir = os.path.join(working_dir, 'source')
     if not os.path.exists(source_dir):
         os.mkdir(source_dir)
-    target_path = os.path.join(source_dir, 'index.rst')
-    shutil.copyfile(target, target_path)
+
+    (target_dir, target) = os.path.split(target_path)
+    (target_name, target_ext) = os.path.splitext(target)
+    print("target_path is {}".format(target_path))
+    print("target_dir is {}".format(target_dir))
+    print("target is {}".format(target))
+    print("target_name is {}".format(target_name))
+    print("target_ext is {}".format(target_ext))
+    new_target_path = os.path.join(source_dir, target)
+    print("new target path is {}".format(new_target_path))
+    shutil.copyfile(target_path, new_target_path)
+
+    for include_path in find_includes(target_path):
+        include_source_path = os.path.join(target_dir, include_path)
+        print("include_source_path: {}".format(include_source_path))
+        include_target_path = os.path.join(source_dir, include_path)
+        include_target_dir = os.path.dirname(include_target_path)
+        print("making: {}".format(include_target_dir))
+        os.makedirs(include_target_dir, exist_ok=True)
+        shutil.copyfile(include_source_path, include_target_path)
+
     conf_path = os.path.join(source_dir, 'conf.py')
     with open(conf_path, 'w') as conf_file:
         conf_file.write(dedent(
             """
-            source_suffix = '.rst'
-            master_doc = 'index'
+            source_suffix = '{}'
+            master_doc = '{}'
             html_theme = 'alabaster'
             html_static_path = ['_static']
-            """
+            """.format(target_ext, target_name)
         ))
 
     builder = Sphinx(source_dir, source_dir, build_dir, build_dir, 'html')
@@ -37,7 +78,9 @@ def create_server(**config):
     Creates a new flask app and wraps it in a livereload server
     """
     working_dir = config.get('WORKING_DIR')
-    target = config.get('TARGET')
+    target_path = config.get('TARGET')
+    (target_dir, target) = os.path.split(target_path)
+    (target_name, target_ext) = os.path.splitext(target)
     template_folder = os.path.join(working_dir, 'build')
     static_folder = os.path.join(template_folder, '_static')
     app = flask.Flask(
@@ -52,7 +95,7 @@ def create_server(**config):
 
     @app.route('/')
     def index():
-        return flask.render_template('index.html')
+        return flask.render_template('{}.html'.format(target_name))
 
     app.logger.debug("configuring livereload server")
     server = Server(app.wsgi_app)
